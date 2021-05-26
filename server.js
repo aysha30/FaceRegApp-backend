4459c0b54955ea68098d405c4d9d171cc3e8f73e
aysha30 <ettashamriya@gmail.com>
2021-05-26 22:23:50 +0530

    database wasnt defined

--- server.js
+++ server.js
@@ -48,9 +48,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res)=> {
-    res.send(database.users);
-})
+app.get('/', (req, res)=> { res.send('it is working') })
 
 app.post('/signin', (req, res) => { signin.handleSignin( req, res, db, bcrypt ) })
 
