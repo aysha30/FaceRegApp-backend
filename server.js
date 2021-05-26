d265151a61411657edccccac922b593d4dd19f53
aysha30 <ettashamriya@gmail.com>
2021-05-26 23:07:04 +0530

    configuring knex database

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'aysha30',
-      password : 'Riya@123',
-      database : 'facereg'
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
     }
 });
 
